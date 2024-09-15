9e5402af4d1c0afe18b0b2becbc26612ac96f75a
MoneyWin153 <2100007107@nttu.edu.vn>
2024-09-15 19:25:17 +0700

    init project

---
+++ hotel/admin/includes/sidebar.php
@@ -0,0 +1,24 @@
+<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i>Home</a></li>
+					<li><a href="#"><i class="fa fa-desktop"></i> Rooms</a>
+					<ul>
+						<li><a href="create-room.php">Add a Room</a></li>
+						<li><a href="manage-rooms.php">Manage Rooms</a></li>
+					</ul>
+				</li>
+						<li><a href="#"><i class="fa fa-files-o"></i> Complaints</a>
+					<ul>
+						<li><a href="new-complaints.php">New</a></li>
+						<li><a href="inprocess-complaints.php">In Process</a></li>
+						<li><a href="closed-complaints.php">Closed</a></li>
+						<li><a href="all-complaints.php">All</a></li>
+					</ul>
+				</li>
+				<li><a href="access-log.php"><i class="fa fa-file"></i>User Access logs</a></li>
+
+			
+		</nav>
+		
